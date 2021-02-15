6c37112ecc8b5a19441c509405cfb89f77da7179
justbydev <wjhappyboy@hanmail.net>
2021-02-15 13:22:07 +0900

    fix model

--- apiserver/models.py
+++ apiserver/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Imgdata(models.Model):
-    imgname=models.CharField(max_length=50)
-    bigcategory=models.CharField(max_length=50)
-    middlecategory=models.CharField(max_length=50)
-    furniturename=models.CharField(max_length=50)
+    imgname=models.CharField(null=True, max_length=50)
+    bigcategory=models.CharField(null=True, max_length=50)
+    middlecategory=models.CharField(null=True, max_length=50)
+    furniturename=models.CharField(null=True, max_length=50)
+    style=models.CharField(null=True, max_length=50)
+    selectfd=models.CharField(null=True, max_length=50)
+    price=models.IntegerField(null=True, blank=False)
+    color=models.IntegerField(null=True, blank=False)
+    detail=models.CharField(null=True, max_length=50)
     image=models.ImageField(blank=True, upload_to="", null=True)
